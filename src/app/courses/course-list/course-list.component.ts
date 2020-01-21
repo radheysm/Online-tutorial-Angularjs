1d0e66d8093bc030bbfba305bf24badc5f094791
radheysm <radheysaraan@gmail.com>
2020-01-21 16:37:15 +0530

    commit

--- src/app/courses/course-list/course-list.component.ts
+++ src/app/courses/course-list/course-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-list.component.css']
 })
 export class CourseListComponent implements OnInit {
-
+ title = "Course List"
   constructor() { }
 
   ngOnInit() {
